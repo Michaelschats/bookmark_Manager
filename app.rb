3e042e73ab5b89654e11c31a3057bc8672e0e86e
Michael Schats <michael_schats@hotmail.com>
2020-11-18 20:23:01 +0000

    Savannah Pair Programme, created a form to ask for user bookmark, and the storing this in /bookmarks after getting info in a form on bookmarks/new

--- app.rb
+++ app.rb
@@ -8,10 +8,19 @@ class BookmarksManager < Sinatra::Base
   end
 
   get '/bookmarks' do
-    p ENV
-    
     @bookmarks = Bookmark.all
     erb(:"bookmarks-list")
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('#{url}')")
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
---
+++ spec/features/adding_bookmarks_spec.rb
@@ -0,0 +1,9 @@
+feature 'Add Bookmarks' do
+  scenario 'user able to add new bookmarks to bookmark manager' do
+    visit('/bookmarks/new')
+    fill_in('url', with: 'http://testbookmark.com')
+    click_button('Add!')
+
+    expect(page).to have_content 'http://testbookmark.com'
+  end
+end
